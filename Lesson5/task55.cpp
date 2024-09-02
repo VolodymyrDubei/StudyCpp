7834cb7ed009b24e4541566aa0e8c9e7d0ac8d12
Volodymyr Dubei <vovandv@gmail.com>
2024-09-03 00:18:03 +0300

    Task55

---
+++ Lesson5/task55.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+float functionDivision(unsigned short int firstVariable, unsigned short int secondVariable);
+
+int main()
+{
+  unsigned short int first, second;
+	float result;
+
+	cout << "\nEnter two numbers.";
+  cout << "\nFirst: ";
+	cin >> first;
+  cout << "\nSecond: ";
+	cin >> second;
+
+	result = functionDivision(first, second);
+
+	cout << "\nFunction result = " << result << endl;
+
+	return 0;
+}
+
+float functionDivision(unsigned short int firstVariable, unsigned short int secondVariable)
+{
+	if (secondVariable == 0)
+		return -1;
+	else
+		return (float)firstVariable / secondVariable;
+}
