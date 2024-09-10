7a4f4cb193821e2bfc8b8875da27e98a0dea295a
Volodymyr Dubei <vovandv@gmail.com>
2024-09-10 22:03:39 +0300

    task74

---
+++ Lesson7/task74.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int index = 100;
+
+	while (index <= 200)
+	{
+    cout << "\nIndex = " << index << endl;
+		index += 2;
+	}
+
+	return 0;
+}
