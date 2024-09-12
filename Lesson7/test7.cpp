ca4158b05860119ba049798cef8e9c5a5fb0b8ac
Volodymyr Dubei <vovandv@gmail.com>
2024-09-13 00:13:00 +0300

    start less 8

---
+++ Lesson7/test7.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int index;
+
+	for (index = 100; index <= 200; index += 2)
+  {
+    cout << "\nIndex = " << index << endl;
+  }
+	
+  return 0;
+}
