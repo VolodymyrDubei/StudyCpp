40f94b539fe344f0b8cbb42244ed2cd23ddd1eb2
Volodymyr Dubei <vovandv@gmail.com>
2024-10-04 20:42:26 +0300

    task121

---
+++ Lesson12/task121.cpp
@@ -0,0 +1,15 @@
+
+
+// Запишіть віртуальний конструктор-копіювальник для класу Square.
+
+class Square : public Rectangle {};
+
+    // Віртуальний конструктор-копіювальник
+    virtual Rectangle* Clone() const {
+        return new Square(*this);
+    }
+
+
+
+
+  
